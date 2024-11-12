e7d8e6c170421cc7d4c7316d928c37f5597f4ef8
Brady Bangasser <bradybbangasser@gmail.com>
2024-11-11 19:04:58 -0600

    Progress on question

--- src/app/questions/[qid]/page.tsx
+++ src/app/questions/[qid]/page.tsx
@@ -1,10 +1,52 @@
 import { useRouter } from "next/router"
 
+interface Answer {
+    correct: bool
+    answer: string
+}
+
+interface Question {
+    question: string
+    answers: Answer[]
+}
+
+const questions: Question[] = [{
+    question: "AHHHHHHHHHHHH",
+    answers: [{
+         correct: false,
+         answer: "0"
+    }, {
+         correct: false,
+         answer: "1",
+    }, {
+         correct: false,
+         answer: "2"
+    }, {
+         correct: false,
+         answer: "3"
+    }]
+}]
+
 export default async function Question({ params } : { params: Promise< { qid: string }> }) {
     const qid = (await params).qid;
+    console.log(questions, questions.length)
+
+    if (qid >= questions.length) {
+        return (
+            <h1>No</h1>
+            )
+    }
+
     return (
         <div>
-            <h1>Question: {qid}</h1>
+            <h1>{questions[qid].question}</h1>
+            {questions[qid].answers.map(answer => {
+                return (
+                    <div key={answer.answer}>
+                        <h2>{answer.answer}</h2>
+                    </div>
+                )
+            })}
         </div>
     )
 }
--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,9 +1,43 @@
+"use client"
+
+interface FAQQuestion {
+    question: string
+    answer: string
+}
+
+const faqQuestions: FAQQuestion[] = [
+    {
+        question: "Why do sites collect my data?",
+        answerHTML: "Nothing is free, websites need to be hosted on a computer somewhere, often through a cloud provider such as AWS or Azure. These platforms are not cheap and cost increases as your web traffic increases. Developers of websites that offer their services for free still need a way to pay for that, the two most common ways to do that are ads and data collection.",
+    }
+]
+
 export default function Question() {
+    window.onkeypress = (event) => {
+        if (event.keyCode !== 0x0D) return
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((pos) => {
+                    console.log(pos)
+                },
+                (err) => {
+                    console.log(err)
+                }
+            )
+        } else {
+
+        }
+    }
+
     return (
-        <div>
+        <div className={``}>
             <h1>Welcome to the Data Game</h1>
             <h3>The only online game that admits to stealing your data</h3>
 
+            <p>The goal of this game is to reach the end of this site without letting it steal data</p>
+            <div>
+            </div>
+
+            <h1>Press Enter to Begin</h1>
         </div>
         )
 }
